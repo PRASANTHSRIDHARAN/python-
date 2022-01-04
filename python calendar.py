103fe0f051b4bbfe6ecd0e510943cd18ffe7c5e7
PRASANTHSRIDHARAN <86358505+PRASANTHSRIDHARAN@users.noreply.github.com>
2022-01-04 09:39:16 +0530

    Update python calendar.py

--- python calendar.py
+++ python calendar.py
@@ -1,6 +1,6 @@
 import  calendar
-month= int(input("enter the month \n"))
-year = int(input("enter the year \n"))
+month= int(input("enter the month "))
+year = int(input("enter the year "))
 
 calendar.setfirstweekday(calendar.SUNDAY)
 mycal=calendar.month(year,month)
